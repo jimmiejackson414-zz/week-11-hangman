08f5fdd922d549312189eb56088a8811fc5bb5da
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-07-07 21:22:41 -0500

    second commit with more word.js additions

--- letter.js
+++ letter.js
@@ -1,10 +1,17 @@
 var Letter = function(let) {
-	//make a charac property and set it to what you think makes second_instructor_demonstration
-
-	//make an appear property and set it to what makes sense
-
-	//make a letterRender property and set it to a function that does what you think makes sense
-
+    //make a charac property and set it to what you think makes second_instructor_demonstration
+    this.charac = let,
+    //make an appear property and set it to what makes sense
+    this.appear = appear,
+    //make a letterRender property and set it to a function that does what you think makes sense
+    this.letterRender = function() {
+        if (this.appear === true) {
+            return this.character;
+        } else {
+            return console.log('false');
+        }
+    }
 };
 
 //export the Letter constructor here
+module.exports = Letter;
--- word.js
+++ word.js
@@ -3,30 +3,42 @@ var Letter = require('./letter.js');
 
 var Word = function(wrd){
 	//set a property called word and set it equal to what you think it should be
-	this.word = word,
+	this.word = wrd,
 	//set a property called lets to an empty array. We will eventually push letter objects in here
 	this.lets = [],
 	//set a property called found to false
 	this.found = false,
 	//make a property called getLets, set it to a function and inside the function loop over the word property and push letter objects into the lets property.
 	this.getLets = function() {
-		for (var i = word.length; i > 0; i++) {
+		for (var i = 0; i < this.word.length; i++) {
 			lets.push(letters[i]);
 		}
 	}
-	//returns ture or false whether we found the current word or not
+	//returns true or false whether we found the current word or not
 	this.didWeFindTheWord = function() {
 		//set the found property to true or false based on whether all the letters have been found or not
+		if () {
 
+		} else {
+
+		}
 		//return the found property
 	};
 
 	this.checkIfLetterFound = function(guessLetter) {
 		//set a variable whatToReturn to 0
-
+		var whatToReturn = 0;
 		//loop over the lets property and if the letter object's charac property equals the guessletter then set the appear property of the letter object to be true. Also increment whatToReturn.
+		for (var i = 0; i < this.lets.length; i++) {
+			if (Letter.let ===   ) {
 
-		//return whatToReturn
+			} else {
+
+			}
+			whatToReturn++;
+		}
+
+		return whatToReturn;
 	};
 
 	this.wordRender = function() {
